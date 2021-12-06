284f144139e8319e08bc752935f9f209691e40ae
ARAN <67265109+ARAN-ai-python@users.noreply.github.com>
2021-12-06 18:01:34 +0900

    三行describe関数
    アップデート内容
    ・バートレット検定を追加
    ・ルビーン検定を追加
    ・偏相関係数を追加
    ・ケンドールの順位相関係数を追加
    ・ケンドール無相関検定を追加
    ・人名を大文字表示に編集

--- describe3.py
+++ describe3.py
@@ -4,6 +4,7 @@ def describe3(data1, data2, data3):
     import numpy as np
     import pandas as pd
     import matplotlib.pyplot as plt
+    from scipy import stats as st
     
     # 元データ出力
     print(data1, data2, data3, sep="\n")
@@ -130,7 +131,7 @@ def describe3(data1, data2, data3):
             cov_s += (d1-m1) * (d2-m2)
         return cov_s / (length(data1)-1)
     
-    # ピアソン相関係数
+    # ピアソンの積率相関係数(Pearson product-moment correlation coefficient)
     # サンプルサイズが異なる場合、Errorを返す
     def pearson_cor(data1, data2):
         if length(data1) != length(data2): return 'Error'
@@ -150,6 +151,15 @@ def describe3(data1, data2, data3):
             return (cor*((length(data1)-2)**0.5)) / ((1-(cor**2))**0.5)
         else:
             return (-cor*(length(data1)-2)**0.5) / ((1-cor**2)**0.5)
+        
+    # 偏相関係数(Partial correlation)
+    # 第三引数のデータの影響を消した第一引数・第二引数のデータ間の相関関係を表す
+    # 正規性を仮定しているので、スピアマンの順位相関係数では使えないはず
+    # サンプルサイズが異なる場合、Errorを返す
+    def partial_cor(data1, data2, data3):
+        r12, r13, r23 = pearson_cor(data1, data2), pearson_cor(data1, data3), pearson_cor(data2, data3)
+        if r12=='Error' or r13=='Error' or r23=='Error': return 'Error'
+        return (r12 - r13*r23) / ((1-r13**2)**0.5 * (1-r23**2)**0.5)
         
     # リストのコピー生成
     def copy(data):
@@ -184,7 +194,7 @@ def describe3(data1, data2, data3):
                 rank_num_list += [rank_num]
         return ranked, rank_num_list
     
-    # スピアマン順位相関係数........................ちょっとだけズレがある
+    # スピアマンの順位相関係数(Spearman's rank correlation coefficient)........................ちょっとだけズレがある
     # サンプルサイズが異なる場合、Errorを返す
     def spearman_cor(data1, data2):
         if length(data1) != length(data2): return 'Error'
@@ -205,6 +215,109 @@ def describe3(data1, data2, data3):
         spearman =  spearman_cor(data1, data2)
         if spearman == 'Error': return 'Error'
         return spearman * ((length(data1)-1)/(1-spearman**2))**0.5
+    
+    # ケンドールの順位相関係数(Kendall's rank correlation coefficient)
+    # サンプルサイズが異なる場合、Errorを返す
+    # ソース：https://manabitimes.jp/math/1286
+    # ソース2：https://bellcurve.jp/statistics/glossary/1307.html
+    def kendall_cor(data1, data2):
+        n1, n2 = length(data1), length(data2)
+        if n1 != n2: return 'Error'
+        
+        plus, minus, zero1, zero2 = 0, 0, 0, 0
+        for i in range(n1-1):
+            for j in range(i+1, n2):
+                d = (data1[i]-data1[j])*(data2[i]-data2[j])
+                if d == 0:
+                    if data1[i] == data1[j]: zero1 += 1
+                    else: zero2 += 1
+                elif d > 0: plus += 1
+                else: minus += 1
+        
+        N = (n1*(n1-1)) / 2
+        T = (plus - minus) / ((N-zero1)**0.5 * (N-zero2)**0.5)
+        p = T / ((2*(2*n1+5)) / (9*n1*(n1-1)))**0.5
+        return T
+    
+    # ケンドール無相関検定(検定統計量)
+    # サンプルサイズが異なる場合、Errorを返す
+    # ソース：https://oceanone.hatenablog.com/entry/2020/04/28/022222
+    def kendall_cor_test(data1, data2):
+        kendall = kendall_cor(data1, data2)
+        if kendall == 'Error': return 'Error'
+        n = length(data1)
+        return kendall / ((2*(2*n+5)) / (9*n*(n-1)))**0.5
+    
+    # コルモゴロフ-スミルノフ検定(Kolmogorov-Smirnov test).......................未完成
+    # 帰無仮説：標本分布が仮定した分布と等しい(今回の場合は正規分布)
+    # 対立仮説：標本分布が仮定した分布と異なる(今回の場合は正規分布)
+#     def kolmogorov_smirnov_test(data):
+#         n = sum_value(data)
+#         l = length(data)
+#         m = mean(data)
+#         std = std_s(data)
+#         dev = n / l
+#         data = data[:-1]
+        
+#         Fo, Ft, FoFt = 0, 0, []
+#         for d in data:
+#             Fo += d / n
+#             Ft += dev / n
+#             FoFt += [Fo - st.norm.cdf(x=Ft, loc=m, scale=std)]
+#             #print(st.norm.cdf(x=Ft, loc=m, scale=std))
+#         FoFt = make_abs(FoFt)
+#         #print(FoFt)
+#         FoFt_max = max_value(FoFt)
+#         return "({:.3f}, {:.3f})".format(FoFt_max, 1.36 / (n)**0.5)
+    
+    # バートレット検定(Bartlett test)
+    # 母集団に正規性がある3群標本間の等分散性を検定する
+    # 帰無仮説：各群の分散は均一である
+    # 対立仮説：各群の分散は均一でない
+    # ソース：https://kusuri-jouhou.com/statistics/bartlett.html
+    def bartlett_test(data1, data2, data3):
+        n1, n2, n3 = length(data1), length(data2), length(data3)
+        v1, v2, v3 = var_s(data1), var_s(data2), var_s(data3)
+        N = n1 + n2 + n3
+        k = 3
+        
+        se2 = ((n1-1)*v1 + (n2-1)*v2 + (n3-1)*v3) / ((n1+n2+n3) - k)
+        M = (N-k)*np.log(se2) - ((n1-1)*np.log(v1) + (n2-1)*np.log(v2) + (n3-1)*np.log(v3))
+        C = 1 + 1/(3*(k-1)) * (1/(n1-1) + 1/(n2-1) + 1/(n3-1) - 1/(N-k))
+        X2 = M / C
+        return X2, k-1
+    
+    # リストの要素を絶対値に直す
+    # ルビーン検定のために実装
+    def make_abs(data):
+        abs_data = []
+        for d in data:
+            if d >= 0: abs_data += [d]
+            else: abs_data += [-d]
+        return abs_data
+    
+    # ルビーン検定(Levene's test)
+    # 母集団に正規性がない3群標本間の等分散性を検定する
+    # 帰無仮説：各群の分散は均一である
+    # 対立仮説：各群の分散は均一でない
+    # ソース：https://ja.wikipedia.org/wiki/ルビーン検定
+    # scipyの計算結果と合わないが、一応このサイトの例題と検定統計量の値が同じになった：https://istat.co.jp/sk_commentary/variance/Rubins-test
+    def levene_test(data1, data2, data3):
+        n1, n2, n3 = length(data1), length(data2), length(data3)
+        m1, m2, m3 = mean(data1), mean(data2), mean(data3)
+        N = n1 + n2 + n3
+        k = 3
+        z1, z2, z3 = make_abs(data1-m1), make_abs(data2-m2), make_abs(data3-m3)
+        zm1, zm2, zm3 = mean(z1), mean(z2), mean(z3)
+        zm = mean(list(z1) + list(z2) + list(z3))
+        
+        sz1, sz2, sz3 = 0, 0, 0
+        for z in z1: sz1 += (z - zm1)**2
+        for z in z2: sz2 += (z - zm2)**2
+        for z in z3: sz3 += (z - zm3)**2
+        
+        W = ((N-k) / (k-1)) * ((n1*(zm1-zm)**2 + n2*(zm2-zm)**2 + n3*(zm3-zm)**2) / (sz1 + sz2 + sz3))
+        return W, "({},{})".format(k-1, N-k)
     
     # 一元配置分散分析(ANalysis Of VAriance)
     # 自由度(dfw, dfb)のF分布に従う
@@ -280,7 +393,7 @@ def describe3(data1, data2, data3):
         data3_dec = data_linked[data1_len+data2_len:]
         return data1_dec, data2_dec, data3_dec, dup
     
-    # クラスカル=ウォリス検定........................ちょっとだけズレがある
+    # クラスカル=ウォリス検定(Kruskal-Wallis test)........................ちょっとだけズレがある
     def kruskalwallis_test(data1, data2, data3):
         l1, l2, l3 = length(data1), length(data2), length(data3)
         ls = l1 + l2 + l3
@@ -321,7 +434,6 @@ def describe3(data1, data2, data3):
         if n!=n2 or n!=n3 or n2!=n3: return 'Error', 'Error'
         k = 3
         data1_ranked, data2_ranked, data3_ranked = rank_index(data1, data2, data3)
-        print(data1_ranked)
         
         r2 = 0
         for data_ranked in [data1_ranked, data2_ranked, data3_ranked]:
@@ -330,11 +442,16 @@ def describe3(data1, data2, data3):
             
         x20 = (12 / (n*k*(k+1))) * r2 - 3*n*(k+1)
         return x20, k-1
+
     
+    bl_x2, bl_df = bartlett_test(data1, data2, data3)
+    le_w, le_df = levene_test(data1, data2, data3)
     anova_f, anova_dfw, anova_dfb = anova(data1, data2, data3)
     rm_anova_f, rm_anova_df_model, rm_anova_df_error = rm_anova(data1, data2, data3)
     kw_h, kw_k = kruskalwallis_test(data1, data2, data3)
     fm_x20, fm_k = friedman_test(data1, data2, data3)
+    #print(st.kstest(data1, st.norm(loc=mean(data1), scale=std_s(data1)).cdf))
+    
     
     df1 = pd.DataFrame({
         'count':length(data1),
@@ -353,14 +470,20 @@ def describe3(data1, data2, data3):
         '25-75%':quartile_range(data1),
         'cov.p':cov_p(data2, data3),
         'cov.s':cov_s(data2, data3),
-        'pearson_cor':pearson_cor(data2, data3),
-        'pearson_cor_test':pearson_cor_test(data2, data3), #自由度n1-n2+2のt分布表を見ること
-        'spearman_cor':spearman_cor(data2, data3),
-        'spearman_cor_test':spearman_cor_test(data2, data3),
-        'anova':anova_f,
-        'rm_anova':rm_anova_f,
-        'kw_test':kw_h,
-        'fm_test':fm_x20
+        'Pearson_cor':pearson_cor(data2, data3),
+        'Pearson_cor_test':pearson_cor_test(data2, data3),
+        'partial_cor':partial_cor(data2, data3, data1),
+        'Spearman_cor':spearman_cor(data2, data3),
+        'Spearman_cor_test':spearman_cor_test(data2, data3),
+        'Kendall_cor':kendall_cor(data2, data3),
+        'Kendall_cor_test':kendall_cor_test(data2, data3),
+        #'Kolmogorov-Smirnov test':kolmogorov_smirnov_test(data1),
+        "Bartlett's test":bl_x2,
+        "Levene's test":le_w,
+        'ANOVA':anova_f,
+        'RM_ANOVA':rm_anova_f,
+        'Kruskal-Wallis test':kw_h,
+        "Friedman's test":fm_x20
     }, index=["data1"]).T
     
     df2 = pd.DataFrame({
@@ -380,14 +503,20 @@ def describe3(data1, data2, data3):
         '25-75%':quartile_range(data2),
         'cov.p':cov_p(data1, data3),
         'cov.s':cov_s(data1, data3),
-        'pearson_cor':pearson_cor(data1, data3),
-        'pearson_cor_test':pearson_cor_test(data1, data3), #自由度n1-n2+2のt分布表を見ること
-        'spearman_cor':spearman_cor(data1, data3),
-        'spearman_cor_test':spearman_cor_test(data1, data3),
-        'anova':anova_dfw,
-        'rm_anova':rm_anova_df_model,
-        'kw_test':kw_k,
-        'fm_test':fm_k
+        'Pearson_cor':pearson_cor(data1, data3),
+        'Pearson_cor_test':pearson_cor_test(data1, data3),
+        'partial_cor':partial_cor(data1, data3, data2),
+        'Spearman_cor':spearman_cor(data1, data3),
+        'Spearman_cor_test':spearman_cor_test(data1, data3),
+        'Kendall_cor':kendall_cor(data1, data3),
+        'Kendall_cor_test':kendall_cor_test(data1, data3),
+        #'Kolmogorov-Smirnov test':kolmogorov_smirnov_test(data2),
+        "Bartlett's test":bl_df,
+        "Levene's test":le_df,
+        'ANOVA':anova_dfw,
+        'RM_ANOVA':rm_anova_df_model,
+        'Kruskal-Wallis test':kw_k,
+        "Friedman's test":fm_k
     }, index=["data2"]).T
     
     df3 = pd.DataFrame({
@@ -407,12 +536,16 @@ def describe3(data1, data2, data3):
         '25-75%':quartile_range(data3),
         'cov.p':cov_p(data1, data2),
         'cov.s':cov_s(data1, data2),
-        'pearson_cor':pearson_cor(data1, data2),
-        'pearson_cor_test':pearson_cor_test(data1, data2), #自由度n1-n2+2のt分布表を見ること
-        'spearman_cor':spearman_cor(data1, data2),
-        'spearman_cor_test':spearman_cor_test(data1, data2),
-        'anova':anova_dfb,
-        'rm_anova':rm_anova_df_error
+        'Pearson_cor':pearson_cor(data1, data2),
+        'Pearson_cor_test':pearson_cor_test(data1, data2),
+        'partial_cor':partial_cor(data1, data2, data3),
+        'Spearman_cor':spearman_cor(data1, data2),
+        'Spearman_cor_test':spearman_cor_test(data1, data2),
+        'Kendall_cor':kendall_cor(data1, data2),
+        'Kendall_cor_test':kendall_cor_test(data1, data2),
+        #'Kolmogorov-Smirnov test':kolmogorov_smirnov_test(data3),
+        'ANOVA':anova_dfb,
+        'RM_ANOVA':rm_anova_df_error
     }, index=["data3"]).T
     
     return display(pd.concat([df1, df2, df3], axis=1))
@@ -422,15 +555,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # ランダム分布
-data1 = pd.DataFrame(np.random.randint(0,11,100), columns=["data1"])["data1"]
-data2 = pd.DataFrame(np.random.randint(0,101,100), columns=["data2"])["data2"]
-data3 = pd.DataFrame(np.random.randint(0,1001,100), columns=["data3"])["data3"]
+data1 = pd.DataFrame(np.random.randint(-100, 101, 100), columns=["data1"])["data1"]
+data2 = pd.DataFrame(np.random.randint(-100, 101, 100), columns=["data2"])["data2"]
+data3 = pd.DataFrame(np.random.randint(-100, 101, 100), columns=["data3"])["data3"]
 describe3(data1, data2, data3)
 
 # 既存ライブラリで検証
 from scipy import stats as st
 print("PearsonrResult" + str(st.pearsonr(data1, data2)))
 print(st.spearmanr(data1, data2))
+print(st.kendalltau(data1, data2))
+print(st.kstest(data1, st.norm(loc=np.mean(data1), scale=np.std(data1)).cdf))
+print(st.bartlett(data1, data2, data3))
+print(st.levene(data1, data2, data3))
 print(st.f_oneway(data1, data2, data3))
 print(st.kruskal(data1, data2, data3))
 print(st.friedmanchisquare(data1, data2, data3))
