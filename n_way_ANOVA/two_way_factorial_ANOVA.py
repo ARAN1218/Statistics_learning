7b709e10ee754a469759a10fa2627ce837fd1f28
ARAN <67265109+ARAN1218@users.noreply.github.com>
2022-02-10 00:57:24 +0900

    Update two_way_factorial_ANOVA.py

--- n_way_ANOVA/two_way_factorial_ANOVA.py
+++ n_way_ANOVA/two_way_factorial_ANOVA.py
@@ -19,14 +19,15 @@ def two_way_factorial_ANOVA(df_list):
     S12 = S1_2 - S1 - S2
     
     # 誤差変動Seを求める
-    S = sum([((df-f_mean)**2).sum().sum() for df in df_list])
-    Se = S - S1 - S2 - S12
+    St = sum([((df-f_mean)**2).sum().sum() for df in df_list])
+    Se = St - S1 - S2 - S12
     
     # 自由度dfを求める
     df1 = (f2_len / df_list_len) - 1
     df2 = (f1_len / df_list_len) - 1
     df12 = df1 * df2
     dfe = (f1_len / df_list_len) * (f2_len / df_list_len) * (df_list_len - 1)
+    dft = df1 + df2 + df12 + dfe
     
     # 不偏分散Vを求める
     V1 = S1 / df1
@@ -45,13 +46,13 @@ def two_way_factorial_ANOVA(df_list):
     p12 = 1 - st.f.cdf(F12, dfn=df12, dfd=dfe)
     
     # 分散分析表を作成する
-    df_S = pd.Series([S1, S2, S12, Se])
-    df_df = pd.Series([df1, df2, df12, dfe])
+    df_S = pd.Series([S1, S2, S12, Se, St])
+    df_df = pd.Series([df1, df2, df12, dfe, dft])
     df_V = pd.Series([V1, V2, V12, Ve])
     df_F = pd.Series([F1, F2, F12])
     df_p = pd.DataFrame([p1, p2, p12], columns=['p'])
     df_p['sign'] = df_p['p'].apply(lambda x : '**' if x < 0.01 else '*' if x < 0.05 else '')
-    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Index', 'Columns', 'Index*Columns', 'Error']).fillna('')
+    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Index', 'Columns', 'Index*Columns', 'Error', 'Total']).fillna('')
 
     # 因子の効果をデータフレームにまとめる
     df_effect = pd.DataFrame(pd.concat([f1_effect, f2_effect])).T.set_axis(['Effect'])
