0c717a7d93a4f9c2f543f068d3dfa5454bfb07da
ARAN <67265109+ARAN1218@users.noreply.github.com>
2022-02-16 22:40:53 +0900

    Create fisher_exact_test.py

---
+++ cross_tabulation/fisher_exact_test.py
@@ -0,0 +1,27 @@
+def fisher_exact_test(df_test):
+    # columnsとindexと全体の合計値を求める
+    sum_columns, sum_index = list(df_test.sum()), list(df_test.sum(axis=1))
+    sum_all = sum(sum_columns)
+    
+    # 階乗を計算する
+    factorial_columns = np.prod([np.math.factorial(i) for i in sum_columns])
+    factorial_index = np.prod([np.math.factorial(i) for i in sum_index])
+    numerator = factorial_columns * factorial_index
+    
+    factorial_list = []
+    for index in df_test.index:
+        for column in df_test.columns:
+            factorial_list.append(np.math.factorial(df_test[column][index]))
+    denominator = np.prod(factorial_list) * np.math.factorial(sum_all)
+    
+    p = numerator / denominator
+    return p
+  
+  
+# テスト
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+df_test = pd.DataFrame([[3,2], [1,4]], columns=['肉が好き', '魚が好き'], index=['女性', '男性'])
+display(df_test)
+fisher_exact_test(df_test)
