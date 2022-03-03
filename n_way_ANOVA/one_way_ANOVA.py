9eb11ce30704b10896c00020f7dc89017bddea57
ARAN <67265109+ARAN1218@users.noreply.github.com>
2022-03-03 21:46:40 +0900

    Update one_way_ANOVA.py
    ・因子の各水準毎の効果について、平均値の他に母平均値の95%信頼区間の出力の追加
    ・各水準毎の取得データの予測値の出力の追加

--- n_way_ANOVA/one_way_ANOVA.py
+++ n_way_ANOVA/one_way_ANOVA.py
@@ -36,13 +36,17 @@ def one_way_ANOVA(df):
     df_F = pd.Series([F])
     df_p = pd.DataFrame([p], columns=['p'])
     df_p['sign'] = df_p['p'].apply(lambda x : '**' if x < 0.01 else '*' if x < 0.05 else '')
-    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Columns','Error', 'Total']).fillna('')
+    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Columns','Error', 'Total']).rename_axis('ANOVA_table', axis=1).fillna('')
     
     # 因子の効果をデータフレームにまとめる
-    df_effect = pd.DataFrame(f1_effect).T.set_axis(['Effect'])
+    df_effect = pd.DataFrame(f1_effect).set_axis(['mean'], axis=1).rename_axis('Effect', axis=1)
+    df_effect['mean(95%CL)'] = df_effect['mean'].map(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/n)))
+    
+    # 各水準毎の取得データの予測値をデータフレームにまとめる
+    df_prediction = pd.DataFrame(df_effect['mean'].map(lambda x : st.t.interval(0.95, dfe, loc=x+f_mean, scale=np.sqrt(Ve/n + Ve)))).T.set_axis(['value']).rename_axis('Prediction(95%CL)', axis=1)
     
     # 結果を出力する
-    return df_ANOVA, df_effect
+    return df_ANOVA, df_effect.T, df_prediction
 
 
 # テスト
@@ -50,10 +54,10 @@ import numpy as np
 import pandas as pd
 import scipy.stats as st
 
-df_1 = pd.DataFrame([[8,10,12], [4,8,12]]).set_axis(['b0', 'b1', 'b2'], axis=1)
+df_1 = pd.DataFrame([[8,10,12], [4,8,12]]).set_axis(['column0', 'column1', 'column2'], axis=1)
 print("入力：")
 display(df_1)
 
-df_ANOVA, df_effect = one_way_ANOVA(df_1)
+df_ANOVA, df_effect, df_prediction = one_way_ANOVA(df_1)
 print("出力：")
-display(df_ANOVA, df_effect)
+display(df_ANOVA, df_effect, df_prediction)
