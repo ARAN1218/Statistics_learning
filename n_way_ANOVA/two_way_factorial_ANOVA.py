7b36411a1d6c1f9f272a10526814989ba06226f6
ARAN <67265109+ARAN1218@users.noreply.github.com>
2022-03-03 21:49:17 +0900

    Update two_way_factorial_ANOVA.py
    ・因子の各水準毎の効果について、平均値の他に母平均値の95%信頼区間の出力の追加
    ・各水準毎の取得データの予測値の出力の追加

--- n_way_ANOVA/two_way_factorial_ANOVA.py
+++ n_way_ANOVA/two_way_factorial_ANOVA.py
@@ -52,13 +52,22 @@ def two_way_factorial_ANOVA(df_list):
     df_F = pd.Series([F1, F2, F12])
     df_p = pd.DataFrame([p1, p2, p12], columns=['p'])
     df_p['sign'] = df_p['p'].apply(lambda x : '**' if x < 0.01 else '*' if x < 0.05 else '')
-    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Index', 'Columns', 'Index*Columns', 'Error', 'Total']).fillna('')
-
+    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Indexes', 'Columns', 'Indexes*Columns', 'Error', 'Total']).rename_axis('ANOVA_table', axis=1).fillna('')
+    
     # 因子の効果をデータフレームにまとめる
-    df_effect = pd.DataFrame(pd.concat([f1_effect, f2_effect])).T.set_axis(['Effect'])
+    df_effect_indexes = pd.DataFrame(f1_effect).set_axis(['mean'], axis=1)
+    df_effect_columns = pd.DataFrame(f2_effect).set_axis(['mean'], axis=1)
+    df_effect_indexes['mean(95%CL)'] = df_effect_indexes['mean'].map(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/f1_len)))
+    df_effect_columns['mean(95%CL)'] = df_effect_columns['mean'].map(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/f2_len)))
+    df_effect = pd.concat([df_effect_indexes, df_effect_columns]).T.rename_axis('Effect', axis=1)
+    
+    # 各水準毎の取得データの予測値をデータフレームにまとめる
+    df_prediction = pd.DataFrame([[f1 + f2 + f_mean for f2 in f2_effect] for f1 in f1_effect])
+    df_prediction = df_prediction.applymap(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/df_list_len + Ve)))
+    df_prediction = df_prediction.set_axis(df_list[0].index).set_axis(df_list[0].columns, axis=1).rename_axis('Prediction(95%CL)', axis=1)
     
     # 結果を出力する
-    return df_ANOVA, df_effect
+    return df_ANOVA, df_effect, df_prediction
 
 
 # テスト
@@ -66,13 +75,13 @@ import numpy as np
 import pandas as pd
 import scipy.stats as st
 
-df_2_yes1 = pd.DataFrame([[8,10,12], [4,8,12]]).set_axis(['b0', 'b1', 'b2'], axis=1).set_axis(['a0', 'a1'], axis=0)
-df_2_yes2 = pd.DataFrame([[6,6,12], [2,4,12]]).set_axis(['b0', 'b1', 'b2'], axis=1).set_axis(['a0', 'a1'], axis=0)
+df_2_yes1 = pd.DataFrame([[8,10,12], [4,8,12]]).set_axis(['column0', 'column1', 'column2'], axis=1).set_axis(['index0', 'index1'], axis=0)
+df_2_yes2 = pd.DataFrame([[6,6,12], [2,4,12]]).set_axis(['column0', 'column1', 'column2'], axis=1).set_axis(['index0', 'index1'], axis=0)
 print("入力：")
 display(df_2_yes1)
 display(df_2_yes2)
 
 df_2_yes_list = [df_2_yes1, df_2_yes2]
-df_ANOVA, df_effect = two_way_factorial_ANOVA(df_2_yes_list)
+df_ANOVA, df_effect, df_prediction = two_way_factorial_ANOVA(df_2_yes_list)
 print("出力：")
-display(df_ANOVA, df_effect)
+display(df_ANOVA, df_effect, df_prediction)
