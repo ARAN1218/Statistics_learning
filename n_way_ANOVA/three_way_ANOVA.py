6af93a77b3f7d37ddaafbfad08aa2687f8e83dc7
ARAN <67265109+ARAN1218@users.noreply.github.com>
2022-03-03 21:58:16 +0900

    Update three_way_ANOVA.py
    ・因子の各水準毎の効果について、平均値の他に母平均値の95%信頼区間の出力の追加
    ・各水準毎の取得データの予測値の出力の追加

--- n_way_ANOVA/three_way_ANOVA.py
+++ n_way_ANOVA/three_way_ANOVA.py
@@ -75,13 +75,27 @@ def three_way_ANOVA(df_list):
     df_F = pd.Series([F1, F2, F3, F12, F13, F23])
     df_p = pd.DataFrame([p1, p2, p3, p12, p13, p23], columns=['p'])
     df_p['sign'] = df_p['p'].apply(lambda x : '**' if x < 0.01 else '*' if x < 0.05 else '')
-    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Index', 'Columns', 'Tables', 'Index*Columns', 'Index*Tables', 'Columns*Tables', 'Error', 'Total']).fillna('')
+    df_ANOVA = pd.concat([df_S, df_df, df_V, df_F, df_p], axis=1).set_axis(['S','df','V','F','p','sign'], axis=1).set_axis(['Indexes', 'Columns', 'Tables', 'Indexes*Columns', 'Indexes*Tables', 'Columns*Tables', 'Error', 'Total']).rename_axis('ANOVA_table', axis=1).fillna('')
     
     # 因子の効果をデータフレームにまとめる
-    df_effect = pd.DataFrame(pd.concat([f1_effect, f2_effect, f3_effect])).T.set_axis(['Effect'])
+    df_effect_indexes = pd.DataFrame(f1_effect).set_axis(['mean'], axis=1)
+    df_effect_columns = pd.DataFrame(f2_effect).set_axis(['mean'], axis=1)
+    df_effect_tables = pd.DataFrame(f3_effect).set_axis(['mean'], axis=1)
+    df_effect_indexes['mean(95%CL)'] = df_effect_indexes['mean'].map(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/f1_len)))
+    df_effect_columns['mean(95%CL)'] = df_effect_columns['mean'].map(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/f2_len)))
+    df_effect_tables['mean(95%CL)'] = df_effect_tables['mean'].map(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/f3_len)))
+    df_effect = pd.concat([df_effect_indexes, df_effect_columns, df_effect_tables]).T.rename_axis('Effect', axis=1)
+    
+    # 各水準毎の取得データの予測値をデータフレームにまとめる
+    df_predictions = []
+    for f3 in f3_effect:
+        df_prediction = pd.DataFrame([[f1 + f2 + f3 + f_mean for f2 in f2_effect] for f1 in f1_effect])
+        df_prediction = df_prediction.applymap(lambda x : st.t.interval(0.95, dfe, loc=x, scale=np.sqrt(Ve/1 + Ve)))
+        df_prediction = df_prediction.set_axis(df_list[0].index).set_axis(df_list[0].columns, axis=1).rename_axis('Prediction(95%CL)', axis=1)
+        df_predictions.append(df_prediction)
     
     # 結果を出力する
-    return df_ANOVA, df_effect
+    return df_ANOVA, df_effect, df_predictions
 
 
 # テスト
@@ -89,7 +103,7 @@ import numpy as np
 import pandas as pd
 import scipy.stats as st
 
-df_3_no = pd.DataFrame([[9,10,12], [4,8,10], [6,9,12], [2,6,13]]).set_axis(['b0', 'b1', 'b2'], axis=1).set_axis(['a0', 'a1', 'a0', 'a1'], axis=0)
+df_3_no = pd.DataFrame([[9,10,12], [4,8,10], [6,9,12], [2,6,13]]).set_axis(['column0', 'column1', 'column2'], axis=1).set_axis(['index0', 'index1', 'index0', 'index1'], axis=0)
 df_3_no_upper = df_3_no.iloc[:2]
 df_3_no_under = df_3_no.iloc[2:]
 print("入力：")
@@ -98,6 +112,6 @@ display(df_3_no_upper)
 display(df_3_no_under)
 
 df_3_no_list = [df_3_no_upper, df_3_no_under]
-df_ANOVA, df_effect = three_way_ANOVA(df_3_no_list)
+df_ANOVA, df_effect, df_predictions = three_way_ANOVA(df_3_no_list)
 print("出力：")
-display(df_ANOVA, df_effect)
+display(df_ANOVA, df_effect, df_predictions)
