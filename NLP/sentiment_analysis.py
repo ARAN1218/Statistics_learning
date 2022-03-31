60816693ba08782524aef9a33b249e72247b1418
ARAN <67265109+ARAN1218@users.noreply.github.com>
2022-03-31 22:00:09 +0900

    Update sentiment_analysis.py

--- NLP/sentiment_analysis.py
+++ NLP/sentiment_analysis.py
@@ -9,6 +9,18 @@ from asari.api import Sonar
 import pandas as pd
 
 
+def sentiment_analysis(df_texts):
+    text_list, positive_list, negative_list, top_list = [], [], [], []
+    for text in df_tests['texts']:
+        result = sonar.ping(text=text)
+        text_list.append(result['text'])
+        positive_list.append(result['classes'][1]['confidence'])
+        negative_list.append(result['classes'][0]['confidence'])
+        top_list.append(result['top_class'])
+    df_result = pd.DataFrame([text_list, positive_list, negative_list, top_list], index=['text','positive','negative','sentiment']).T
+    display(df_result)
+
+
 # テスト
 sonar = Sonar()
 df_tests = pd.DataFrame([
@@ -20,13 +32,7 @@ df_tests = pd.DataFrame([
     ['私はこれが好きじゃなくなくなくなくなくなくなくなくなくなくなくない。']
 ], columns=['texts'])
 
-text_list, positive_list, negative_list, top_list = [], [], [], []
-for text in df_tests['texts']:
-    result = sonar.ping(text=text)
-    text_list.append(result['text'])
-    positive_list.append(result['classes'][1]['confidence'])
-    negative_list.append(result['classes'][0]['confidence'])
-    top_list.append(result['top_class'])
-
-df_result = pd.DataFrame([text_list, positive_list, negative_list, top_list], index=['text','positive','negative','sentiment']).T
-df_result
+print("入力：")
+display(df_texts)
+print("出力：")
+sentiment_analysis(df_tests)
